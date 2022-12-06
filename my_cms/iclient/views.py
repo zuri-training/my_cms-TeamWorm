9b117bed1f347aa53e08e9a0208980ee6cf081c8
Desmondflexy <ikilodesmond@gmail.com>
2022-12-06 22:47:24 +0100

    removed iclient app from project

--- my_cms/iclient/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-
-def index(request):
-    return render(request, 'iclient/index.html', {
-        # to-be-edited: key-value pairs; django site variables,
-    })
