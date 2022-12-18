be8a99ed75044360ecd182b0fecdeae834d4d0a5
Desmondflexy <ikilodesmond@gmail.com>
2022-12-19 00:34:41 +0100

    added landing page

--- frontend/scripts/form.js
+++ frontend/scripts/form.js
@@ -1,13 +1,13 @@
 const inputs = document.querySelectorAll('input');
 
 inputs.forEach(input => {
-    input.oninput = ()=>{
-        if (!input.validity.valid){
+    input.oninput = () => {
+        if (!input.validity.valid) {
             input.style.outline = '1.5px solid red';
-        } else{
+        } else {
             input.style.outline = '1.5px solid green';
         }
-        if (input.value.length < 1){
+        if (input.value.length < 1) {
             input.style.outline = '1.5px solid black';
         }
     }
