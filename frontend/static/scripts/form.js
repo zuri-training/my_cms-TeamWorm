734e466de1ef60f859f1b4f617b719fec5ccb84f
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2022-12-30 02:05:27 +0100

    add files

---
+++ frontend/static/scripts/form.js
@@ -0,0 +1,14 @@
+const inputs = document.querySelectorAll('input');
+
+inputs.forEach(input => {
+    input.oninput = () => {
+        if (!input.validity.valid) {
+            input.style.outline = '1.5px solid red';
+        } else {
+            input.style.outline = '1.5px solid green';
+        }
+        if (input.value.length < 1) {
+            input.style.outline = '1.5px solid black';
+        }
+    }
+})
